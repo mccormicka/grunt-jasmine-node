46c7a95baf164e5425a69ad50f4cfe630ab5009d
Anthony McCormick <=>
2013-04-28 12:03:04 +0100

    Enabling grunt-jasmine-node to use jscoverage from custom jasmine-node build

--- tasks/jasmine-node-task.js
+++ tasks/jasmine-node-task.js
@@ -1,85 +1,93 @@
 module.exports = function (grunt) {
     'use strict';
 
-    grunt.registerMultiTask("jasmine_node", "Runs jasmine-node.", function() {
-      var jasmine = require('jasmine-node');
-      var util;
-      try {
-          util = require('util');
-      } catch(e) {
-          util = require('sys');
-      }
-
-      var options = this.options({
-        match: '.',
-        matchall: false,
-        specNameMatcher: 'spec',
-        helperNameMatcher: 'helpers',
-        extensions: 'js',
-        showColors: true,
-        includeStackTrace: true,
-        useHelpers: false,
-        teamcity: false,
-        coffee: false,
-        verbose: false,
-        jUnit: {
-          report: false,
-          savePath : "./reports/",
-          useDotNotation: true,
-          consolidate: true
+    grunt.registerMultiTask("jasmine_node", "Runs jasmine-node.", function () {
+        var jasmine = require('jasmine-node');
+        var util;
+        try {
+            util = require('util');
+        } catch (e) {
+            util = require('sys');
         }
-      });
 
-      // Tell grunt this task is asynchronous.
-      var done = this.async();
-
-      if(options.coffee){
-        options.extensions = 'js|coffee|litcoffee';
-        require('coffee-script');     
-      }
-      var regExpSpec = new RegExp(options.match + (options.matchall ? "" : options.specNameMatcher + "\\.") + "(" + options.extensions + ")$", 'i');
-
-      var onComplete = function(runner, log) {
-        var exitCode;
-        util.print('\n');
-        if (runner.results().failedCount === 0) {
-          exitCode = 0;
-        } else {
-          exitCode = 1;
-
-          if (options.forceExit) {
-            process.exit(exitCode);
-          }
-        }
-        jasmine.getGlobal().jasmine.currentEnv_ = undefined;
-        done();
-      };
-
-      if (options.useHelpers) {
-        this.filesSrc.forEach(function(path){
-          jasmine.loadHelpersInFolder(path, new RegExp(options.helperNameMatcher + "?\\.(" + options.extensions + ")$", 'i'));
+        var options = this.options({
+            match: '.',
+            matchall: false,
+            specNameMatcher: 'spec',
+            helperNameMatcher: 'helpers',
+            extensions: 'js',
+            showColors: true,
+            includeStackTrace: true,
+            useHelpers: false,
+            teamcity: false,
+            coffee: false,
+            verbose: false,
+            coverage: {
+                cover: false,
+                savePath: './reports/coverage/',
+                basePath: false,
+                excludes: false
+            },
+            jUnit: {
+                report: false,
+                savePath: "./reports/",
+                useDotNotation: true,
+                consolidate: true
+            }
         });
-      }
 
-      var jasmineOptions = {
-        specFolders: this.filesSrc,
-        onComplete: onComplete,
-        isVerbose: grunt.verbose?true:options.verbose,
-        showColors: options.showColors,
-        teamcity: options.teamcity,
-        useRequireJs: options.useRequireJs,
-        regExpSpec: regExpSpec,
-        junitreport: options.jUnit,
-        includeStackTrace: options.includeStackTrace,
-        coffee: options.coffee
-      };
+        // Tell grunt this task is asynchronous.
+        var done = this.async();
 
-      try {
-        // since jasmine-node@1.0.28 an options object need to be passed
-        jasmine.executeSpecsInFolder(jasmineOptions);
-      } catch (e) {
-        console.log('Failed to execute "jasmine.executeSpecsInFolder": ' + e.stack);
-      }
+        if (options.coffee) {
+            options.extensions = 'js|coffee|litcoffee';
+            require('coffee-script');
+        }
+        var regExpSpec = new RegExp(options.match + (options.matchall ? "" : options.specNameMatcher + "\\.") + "(" + options.extensions + ")$", 'i');
+
+        var onComplete = function (runner, log) {
+            var exitCode;
+            util.print('\n');
+            if (runner.results().failedCount === 0) {
+                exitCode = 0;
+            } else {
+                exitCode = 1;
+
+                if (options.forceExit) {
+                    process.exit(exitCode);
+                }
+            }
+            jasmine.getGlobal().jasmine.currentEnv_ = undefined;
+            done();
+        };
+
+        if (options.useHelpers) {
+            this.filesSrc.forEach(function (path) {
+                jasmine.loadHelpersInFolder(path, new RegExp(options.helperNameMatcher + "?\\.(" + options.extensions + ")$", 'i'));
+            });
+        }
+
+        var jasmineOptions = {
+            specFolders: this.filesSrc,
+            onComplete: onComplete,
+            isVerbose: grunt.verbose ? true : options.verbose,
+            showColors: options.showColors,
+            teamcity: options.teamcity,
+            useRequireJs: options.useRequireJs,
+            regExpSpec: regExpSpec,
+            junitreport: options.jUnit,
+            includeStackTrace: options.includeStackTrace,
+            coffee: options.coffee,
+            coverage: options.coverage
+        };
+
+        try {
+            // since jasmine-node@1.0.28 an options object need to be passed
+            jasmine.setupCoverage(jasmineOptions.coverage);
+            jasmine.executeSpecsInFolder(jasmineOptions);
+        } catch (e) {
+            console.log('Failed to execute "jasmine.executeSpecsInFolder": ' + e.stack);
+        }
 
     });
 };
